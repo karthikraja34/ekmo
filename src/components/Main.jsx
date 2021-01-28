989a75b9856b72722bb3bc5e2ed44c438f933db5
hariads <adshari22@gmail.com>
2021-01-29 00:34:31 +0530

    Download pdf of the invoice preview

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import parse from "html-react-parser";
-import {extractContext, renderHandleBar} from "../utils/misc";
+import axios from 'axios';
+import { extractContext, renderHandleBar } from "../utils/misc";
 import template from "../templates/default/template";
 import data from "../templates/default/data.json";
+import printicon from "../assets/print.svg"
 
 class MainComponent extends Component {
   constructor(props) {
@@ -14,15 +16,38 @@ class MainComponent extends Component {
   }
 
   onChange = (e) => {
-    const data = {...this.state.data}
+    const data = { ...this.state.data }
     if (e.target.dataset && e.target.dataset.type === "repeater") {
       const index = e.target.dataset.index;
-      data["repeater"][index-1][e.target.name].value = e.target.value;
+      data["repeater"][index - 1][e.target.name].value = e.target.value;
     } else {
       data[e.target.name].value = e.target.value;
     }
 
-    this.setState({data})
+    this.setState({ data })
+  }
+
+  printPDF = () => {   
+    //for temp loading
+    document.body.classList.add("cursor-wait")
+
+    const invoicePreview = document.getElementById('invoice').innerHTML;
+    return axios.get(`http://localhost:4000/getPDF`, {
+      responseType: 'arraybuffer',
+      headers: {
+        'Accept': 'application/pdf'
+      },
+      params: { invoicePreview }
+    }).then((response) => {
+      document.body.classList.remove("cursor-wait")
+      const blob = new Blob([response.data], { type: 'application/pdf' })
+      const link = document.createElement('a')
+      link.href = window.URL.createObjectURL(blob)
+      link.download = `invoice.pdf`
+      link.click()
+    })
+      .catch(err => {console.log(err)})
+
   }
 
   render() {
@@ -30,20 +55,23 @@ class MainComponent extends Component {
 
     return (
       <div>
-        {parse(rawHTML, {
-          replace: domNode => {
-            if (domNode.name === "input" || domNode.name === "textarea") {
-              domNode.attribs.onChange = this.onChange;
+        <div id="invoice">
+          {parse(rawHTML, {
+            replace: domNode => {
+              if (domNode.name === "input" || domNode.name === "textarea") {
+                domNode.attribs.onChange = this.onChange;
+              }
+              return domNode
             }
-            return domNode
-          }
-        })}
+          })}
+        </div>
         <div className="text-center">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => {
-          this.setState({previewMode: !this.state.previewMode})
-        }}>
-          Toggle Preview Mode
-        </button>
+            this.setState({ previewMode: !this.state.previewMode })
+          }}>
+            Toggle Preview Mode
+          </button>
+          {this.state.previewMode ? <img src={printicon} alt="print pdf" className="w-11 float-right cursor-pointer" onClick={this.printPDF} /> : <React.Fragment></React.Fragment>}
         </div>
       </div>
     );
