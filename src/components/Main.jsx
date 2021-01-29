2b1889cc652d4c1858152f20895bd67b44f03fa7
hariads <adshari22@gmail.com>
2021-01-29 20:48:39 +0530

    Added functionality to download invoice as pdf

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import parse from "html-react-parser";
+import axios from 'axios';
 import {extractContext, renderHandleBar} from "../utils/misc";
 import template from "../templates/default/template";
 import data from "../templates/default/data.json";
+import printicon from "../assets/print.svg";
 
 class MainComponent extends Component {
   constructor(props) {
@@ -39,27 +41,53 @@ class MainComponent extends Component {
     }
   }
 
+  printPDF = () => {
+    //for temp loading
+    document.body.classList.add("cursor-wait")
+
+    const invoicePreview = document.getElementById('invoice').innerHTML;
+    return axios.get(`http://localhost:4000/getPDF`, {
+      responseType: 'arraybuffer',
+      headers: {
+        'Accept': 'application/pdf'
+      },
+      params: { invoicePreview }
+    }).then((response) => {
+      document.body.classList.remove("cursor-wait")
+      const blob = new Blob([response.data], { type: 'application/pdf' })
+      const link = document.createElement('a')
+      link.href = window.URL.createObjectURL(blob)
+      link.download = `invoice.pdf`
+      link.click()
+    })
+      .catch(err => { console.log(err) })
+
+  }
+
   render() {
     const rawHTML = renderHandleBar(template, extractContext(this.state.data, this.state.previewMode))
     return (
       <div>
-        {parse(rawHTML, {
-          replace: domNode => {
-            if (domNode.name === "input" || domNode.name === "textarea") {
-              domNode.attribs.onChange = this.onChange;
+        <div id="invoice">
+          {parse(rawHTML, {
+            replace: domNode => {
+              if (domNode.name === "input" || domNode.name === "textarea") {
+                domNode.attribs.onChange = this.onChange;
+              }
+              if (domNode.name === "button") {
+                domNode.attribs.onClick = this.onClick;
+              }
+              return domNode
             }
-            if (domNode.name === "button") {
-              domNode.attribs.onClick = this.onClick;
-            }
-            return domNode
-          }
-        })}
+          })}
+        </div>
         <div className="text-center">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => {
-          this.setState({previewMode: !this.state.previewMode})
-        }}>
-          Toggle Preview Mode
-        </button>
+            this.setState({ previewMode: !this.state.previewMode })
+          }}>
+            Toggle Preview Mode
+          </button>
+          {this.state.previewMode ? <img src={printicon} alt="print pdf" className="w-11 float-right cursor-pointer" onClick={this.printPDF} /> : <React.Fragment></React.Fragment>}
         </div>
       </div>
     );
